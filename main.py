01a2381e45a6693d4f96ace82510a02b87c46a38
s257241 <lorenzo.elia@studenti.units.it>
2023-08-24 14:50:35 +0200

    Correct set of ODEs.

--- main.py
+++ main.py
@@ -12,15 +12,14 @@ delta = 0.01     # Demographic balanced birth and death rate
 initial_population = 1000  # Total population size
 
 # System of ODEs
-def sirs_model(y, t, beta, gamma, lambda_val, alpha, sigma, delta):
-    S, I, R, V = y
+def sirs_model(y, t, beta, gamma, alpha, sigma, delta):
+    S, I, R = y
 
-    dSdt = delta - (beta * S * I) / (S + I + R + V) + lambda_val * R - sigma * S
-    dIdt = (beta * S * I) / (S + I + R + V) - gamma * I - alpha * I - delta * I
-    dRdt = gamma * I + alpha * I - lambda_val * R - delta * R
-    dVdt = sigma * S + alpha * R - delta * V
+    dSdt = - (beta * S * I) / (S + I + R) - (sigma + delta) * S + alpha * R + delta * (S + I + R)
+    dIdt = (beta * S * I) / (S + I + R) - (gamma + delta) * I
+    dRdt = sigma * S + gamma * I - (alpha + delta) * R
 
-    return [dSdt, dIdt, dRdt, dVdt]
+    return [dSdt, dIdt, dRdt]
 
 # Initial conditions
 S0 = 0.99 * initial_population
@@ -32,15 +31,14 @@ V0 = 0.0
 t = np.linspace(0, 100, 1000)
 
 # Solve the ODEs
-solution = odeint(sirs_model, [S0, I0, R0, V0], t, args=(beta, gamma, lambda_val, alpha, sigma, delta))
-S, I, R, V = solution.T
+solution = odeint(sirs_model, [S0, I0, R0], t, args=(beta, gamma, alpha, sigma, delta))
+S, I, R = solution.T
 
 # Plot the results
 plt.figure(figsize=(10, 6))
 plt.plot(t, S, label='Susceptible')
 plt.plot(t, I, label='Infectious')
 plt.plot(t, R, label='Recovered')
-plt.plot(t, V, label='Vaccinated')
 plt.xlabel('Time')
 plt.ylabel('Count')
 plt.title('SIRS Model Simulation as ODEs')
